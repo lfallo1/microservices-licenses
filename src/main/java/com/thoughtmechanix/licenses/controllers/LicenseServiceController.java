730341de825ed8e4ab04e3d5323abf8fbfae5578
Lance Fallon <fallon.lance@gmail.com>
2018-08-09 11:12:28 -0400

    test

--- src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -20,7 +20,7 @@ public class LicenseServiceController {
 //        return new License()
 //                .withId(licenseId)
 //                .withOrganizationId(organizationId)
-//                .withProductName("Telecorp")
+//                .withProductName("Telecorpe")
 //                .withLicenseType("Seat")
 //                .withOrganizationId("TestOrganization");
     }
